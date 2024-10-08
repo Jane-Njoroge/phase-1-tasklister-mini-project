7330ddad07d9d369fef57fd0b210c1bd924724cc
Jane-Njoroge <njorogejwangui@gmail.com>
2024-10-08 13:13:13 +0300

    task manager

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+    const taskList = document.getElementById('tasks');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById('new-task-description').value;
+
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskDescription;
+
+    taskList.appendChild(taskItem);
+
+    form.reset();
+});
 });
